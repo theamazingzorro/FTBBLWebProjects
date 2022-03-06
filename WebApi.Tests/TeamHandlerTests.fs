3b0d09d50c42fb45e5316f036da86d1e54d32f40
Fox Noland <theamazingzorro@gmail.com>
2022-03-06 18:45:40 -0500

    test cleanup

--- WebApi.Tests/TeamHandlerTests.fs
+++ WebApi.Tests/TeamHandlerTests.fs
@@ -10,49 +10,48 @@ open Newtonsoft.Json
 
 [<Fact>]
 let ``getTeams returns a list of teams`` () =
-    let expectedData: Team list = [
-            { Name="Team 1"; Race="Lizardmen"; Coach="Theamazingzorro"; IsActive=true }
-            { Name="Team 2"; Race="Necromantic"; Coach="Danean"; IsActive=true }
+    let testTeams: Team list = [
+            { Name="Team 1"; Race="Lizardmen"; Coach="Coach 1"; IsActive=true }
+            { Name="Team 2"; Race="Necromantic"; Coach="Coach 2"; IsActive=true }
         ]
 
     let teamRepoMock() = 
-        expectedData
+        testTeams
 
     let handler = TeamApiHandlers.getTeams teamRepoMock 
-    
     let context = buildMockContext()
 
     task {
         let! response = handler next context
         Assert.True(response.IsSome)
 
-        let body = getBody (response.Value)
-            
-        let data = JsonConvert.DeserializeObject<List<Team>>(body)
+        let data = 
+            response.Value
+            |> getBody
+            |> JsonConvert.DeserializeObject<List<Team>>
 
-        Assert.Equal<List<Team>>(expectedData, data)
+        Assert.Equal<List<Team>>(testTeams, data)
     }
     
 [<Fact>]
 let ``getTeam returns a single team with the given id`` () =
-    let testTeam = { Name="Team 1"; Race="Lizardmen"; Coach="Theamazingzorro"; IsActive=true }
-            
-
+    let testTeam = { Name="Team 1"; Race="Lizardmen"; Coach="Coach 1"; IsActive=true }
+         
     let teamRepoMock(id: int) = 
         Assert.Equal(1, id)
         testTeam
 
     let handler = TeamApiHandlers.getTeam 1 teamRepoMock 
-    
     let context = buildMockContext()
 
     task {
         let! response = handler next context
         Assert.True(response.IsSome)
 
-        let body = getBody (response.Value)
-            
-        let data = JsonConvert.DeserializeObject<Team>(body)
+        let data = 
+            response.Value
+            |> getBody
+            |> JsonConvert.DeserializeObject<Team>
 
         Assert.Equal(testTeam, data)
     }
