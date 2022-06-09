5378c118a40aa1d751b9e618a86a6b6708f65ba0
Fox Noland <theamazingzorro@gmail.com>
2022-06-09 18:24:53 -0400

    sign in page - api support still missing

--- Frontend/src/Page/Signin.elm
+++ Frontend/src/Page/Signin.elm
@@ -1,8 +1,17 @@
 module Page.Signin exposing (Model, Msg, init, update, view)
 
+import Custom.Attributes
+import Custom.Events exposing (onEnter)
+import Error exposing (buildErrorMessage)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Http
 import Model.Session exposing (Session)
+import Model.UserPassword exposing (..)
+import Platform exposing (Router)
+import Route exposing (Route)
+
 
 
 -- Types --
@@ -10,11 +19,16 @@ import Model.Session exposing (Session)
 
 type alias Model =
     { session : Session
+    , userPassword : UserPassword
+    , signinError : Maybe String
     }
 
 
 type Msg
-    = None
+    = UsernameChanged String
+    | PasswordChanged String
+    | Submit
+    | Submitted (Result Http.Error String)
 
 
 
@@ -23,7 +37,12 @@ type Msg
 
 init : Session -> ( Model, Cmd Msg )
 init session =
-    ( { session = session }, Cmd.none )
+    ( { session = session
+      , userPassword = defaultUserPassword
+      , signinError = Nothing
+      }
+    , Cmd.none
+    )
 
 
 
@@ -33,14 +52,109 @@ init session =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        None ->
-            ( model, Cmd.none )
+        UsernameChanged newName ->
+            let
+                rename oldUser =
+                    { oldUser | username = newName }
+            in
+            ( { model | userPassword = rename model.userPassword }, Cmd.none )
+
+        PasswordChanged newPassword ->
+            let
+                repass oldUser =
+                    { oldUser | password = newPassword }
+            in
+            ( { model | userPassword = repass model.userPassword }, Cmd.none )
+
+        Submit ->
+            ( model, signinAttempt model.userPassword )
+
+        Submitted (Ok _) ->
+            ( { model | signinError = Nothing }, Route.pushUrl model.session.navkey Route.Home )
+
+        Submitted (Err err) ->
+            ( { model | signinError = Just (buildErrorMessage err) }, Cmd.none )
+
+
 
 -- API Requests --
+
+
+signinAttempt : UserPassword -> Cmd Msg
+signinAttempt userPassword =
+    Cmd.none
+
+
 
 -- View --
 
 
 view : Model -> Html Msg
-view _ =
-    text "skeleton"
+view model =
+    div []
+        [ h3 [] [ text "Sign in" ]
+        , br [] []
+        , viewSigninError model.signinError
+        , viewForm model
+        ]
+
+
+viewSigninError : Maybe String -> Html msg
+viewSigninError maybeError =
+    case maybeError of
+        Just error ->
+            div [ Custom.Attributes.errorMessage ]
+                [ h3 [] [ text "Couldn't sign in at this time." ]
+                , text ("Error: " ++ error)
+                , br [] []
+                ]
+
+        Nothing ->
+            text ""
+
+
+viewForm : Model -> Html Msg
+viewForm model =
+    div []
+        [ viewUsernameField model.userPassword
+        , viewPasswordField model.userPassword
+        , button
+            [ Custom.Attributes.submitButton
+            , onClick Submit
+            ]
+            [ text "Sign in" ]
+        ]
+
+
+viewUsernameField : UserPassword -> Html Msg
+viewUsernameField userPassword =
+    div [ Custom.Attributes.formEntry ]
+        [ label
+            (Custom.Attributes.formLabel "usernameInput")
+            [ text "User Name" ]
+        , input
+            (Custom.Attributes.formInput "usernameInput"
+                [ onInput UsernameChanged
+                , onEnter Submit
+                , value userPassword.username
+                ]
+            )
+            []
+        ]
+
+
+viewPasswordField : UserPassword -> Html Msg
+viewPasswordField userPassword =
+    div [ Custom.Attributes.formEntry ]
+        [ label
+            (Custom.Attributes.formLabel "passwordInput")
+            [ text "Password" ]
+        , input
+            (Custom.Attributes.formInput "passwordInput"
+                [ onInput PasswordChanged
+                , onEnter Submit
+                , value userPassword.password
+                ]
+            )
+            []
+        ]
