a976bd9473bbc1d4fa448442cc2aa420b418235f
Fox Noland <theamazingzorro@gmail.com>
2024-02-23 23:54:55 -0500

    simple history display and team selection

--- Frontend/src/Page/ViewHeadToHead.elm
+++ Frontend/src/Page/ViewHeadToHead.elm
@@ -1,14 +1,19 @@
 module Page.ViewHeadToHead exposing (Model, Msg, init, update, view)
 
 import Api
+import Custom.Attributes
+import Error
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 import Http
 import Model.Coach as Coach exposing (Coach, CoachId, coachsDecoder)
+import Model.Division exposing (Division, DivisionId, compareDivisions)
 import Model.Game exposing (Game, gamesDecoder)
 import Model.Session exposing (Session)
 import Model.Team as Team exposing (Team, TeamId, defaultTeam, teamsDecoder)
 import RemoteData exposing (WebData)
+import Route exposing (pushUrl)
 
 
 
@@ -24,6 +29,7 @@ type alias Model =
     , games : WebData (List Game)
     , teams : WebData (List Team)
     , coaches : WebData (List Coach)
+    , useTeams : Bool
     }
 
 
@@ -35,6 +41,8 @@ type Msg
     | Team2Selected String
     | Coach1Selected String
     | Coach2Selected String
+    | TeamsCoachSwitch
+    | ViewDivisionButtonClick DivisionId
 
 
 
@@ -53,6 +61,7 @@ init session teams coaches =
               , games = RemoteData.Loading
               , teams = RemoteData.Loading
               , coaches = RemoteData.Loading
+              , useTeams = True
               }
             , Cmd.batch
                 [ getGamesForTeamsRequest session.token team1 team2
@@ -70,6 +79,7 @@ init session teams coaches =
               , games = RemoteData.Loading
               , teams = RemoteData.Loading
               , coaches = RemoteData.Loading
+              , useTeams = False
               }
             , Cmd.batch
                 [ getGamesForCoachesRequest session.token coach1 coach2
@@ -87,6 +97,7 @@ init session teams coaches =
               , games = RemoteData.NotAsked
               , teams = RemoteData.Loading
               , coaches = RemoteData.Loading
+              , useTeams = True
               }
             , Cmd.batch
                 [ getTeamListRequest session.token
@@ -103,7 +114,7 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GamesReceived response ->
-            ( { model | games = response }, Cmd.none )
+            ( { model | games = sortGames response }, Cmd.none )
 
         TeamListReceived response ->
             ( { model | teams = response }, Cmd.none )
@@ -112,10 +123,38 @@ update msg model =
             ( { model | coaches = response }, Cmd.none )
 
         Team1Selected teamId ->
-            ( { model | team1Id = searchForIdString teamId Team.idToString model.teams }, Cmd.none )
+            let
+                team1Id =
+                    searchForIdString teamId Team.idToString model.teams
+
+                ( loading, cmd ) =
+                    maybeGetGamesForTeams model.session.token team1Id model.team2Id
+
+                newGames =
+                    if loading then
+                        RemoteData.Loading
+
+                    else
+                        model.games
+            in
+            ( { model | team1Id = team1Id, games = newGames }, cmd )
 
         Team2Selected teamId ->
-            ( { model | team2Id = searchForIdString teamId Team.idToString model.teams }, Cmd.none )
+            let
+                team2Id =
+                    searchForIdString teamId Team.idToString model.teams
+
+                ( loading, cmd ) =
+                    maybeGetGamesForTeams model.session.token model.team1Id team2Id
+
+                newGames =
+                    if loading then
+                        RemoteData.Loading
+
+                    else
+                        model.games
+            in
+            ( { model | team2Id = team2Id, games = newGames }, cmd )
 
         Coach1Selected coachId ->
             ( { model | coach1Id = searchForIdString coachId Coach.idToString model.coaches }, Cmd.none )
@@ -123,9 +162,47 @@ update msg model =
         Coach2Selected coachId ->
             ( { model | coach2Id = searchForIdString coachId Coach.idToString model.coaches }, Cmd.none )
 
+        TeamsCoachSwitch ->
+            ( { model | useTeams = not model.useTeams }, Cmd.none )
+
+        ViewDivisionButtonClick divId ->
+            ( model, pushUrl model.session.navkey <| Route.ViewDivision divId )
+
+
+sortGames : WebData (List Game) -> WebData (List Game)
+sortGames gameData =
+    case gameData of
+        RemoteData.Success games ->
+            RemoteData.Success <| List.sortWith (\a b -> compareDivisions b.division a.division) games
+
+        other ->
+            other
+
+
+searchForIdString : String -> (id -> String) -> RemoteData.RemoteData e (List { d | id : id }) -> Maybe id
+searchForIdString idString idToString listData =
+    case listData of
+        RemoteData.Success list ->
+            List.filter (\x -> idToString x.id == idString) list
+                |> List.head
+                |> Maybe.map .id
+
+        _ ->
+            Nothing
+
 
 
 -- API Requests --
+
+
+maybeGetGamesForTeams : Maybe String -> Maybe TeamId -> Maybe TeamId -> ( Bool, Cmd Msg )
+maybeGetGamesForTeams token t1 t2 =
+    case ( t1, t2 ) of
+        ( Just team1, Just team2 ) ->
+            ( True, getGamesForTeamsRequest token team1 team2 )
+
+        ( _, _ ) ->
+            ( False, Cmd.none )
 
 
 getGamesForTeamsRequest : Maybe String -> TeamId -> TeamId -> Cmd Msg
@@ -150,18 +227,6 @@ getCoachListRequest : Maybe String -> Cmd Msg
 getCoachListRequest token =
     Api.getRequest token Api.Coaches <|
         Http.expectJson (RemoteData.fromResult >> CoachListReceived) coachsDecoder
-
-
-searchForIdString : String -> (id -> String) -> RemoteData.RemoteData e (List { d | id : id }) -> Maybe id
-searchForIdString idString idToString listData =
-    case listData of
-        RemoteData.Success list ->
-            List.filter (\x -> idToString x.id == idString) list
-                |> List.head
-                |> Maybe.map .id
-
-        _ ->
-            Nothing
 
 
 
@@ -195,5 +260,160 @@ getCoach coachData coachId =
 
 
 view : Model -> Html Msg
-view _ =
-    text "skeleton"
+view model =
+    if model.useTeams then
+        div []
+            [ br [] []
+            , viewTeamSelector model.teams model.team1Id model.team2Id
+            , br [] []
+            , viewGames model.games
+            ]
+
+    else
+        text "coach"
+
+
+viewLoadError : String -> Html Msg
+viewLoadError errorMessage =
+    let
+        errorHeading =
+            "Couldn't fetch data at this time."
+    in
+    div [ Custom.Attributes.errorMessage ]
+        [ h3 [] [ text errorHeading ]
+        , text <| "Error: " ++ errorMessage
+        ]
+
+
+
+-- View Games List --
+
+
+viewGames : WebData (List Game) -> Html Msg
+viewGames gameData =
+    case gameData of
+        RemoteData.Success games ->
+            div []
+                [ h4 [ Custom.Attributes.textCentered ]
+                    [ text "History" ]
+                , div []
+                    (List.map viewGameDetails games)
+                ]
+
+        RemoteData.NotAsked ->
+            text ""
+
+        RemoteData.Loading ->
+            h3 [] [ text "Loading Games..." ]
+
+        RemoteData.Failure httpError ->
+            viewLoadError <| Error.buildErrorMessage httpError
+
+
+viewGameDetails : Game -> Html Msg
+viewGameDetails game =
+    div Custom.Attributes.carouselItemEntry
+        [ div [ class "row" ]
+            [ h6 [] [ viewDivision game.division ] ]
+        , div [ class "row" ]
+            [ div [ class "col" ]
+                [ h6 [] [ text game.homeTeam.name ]
+                , p [] [ text game.homeTeam.coach.name ]
+                ]
+            , div [ class "col col-auto", Custom.Attributes.centered ]
+                [ viewScore game
+                ]
+            , div [ class "col" ]
+                [ h6 [] [ text game.awayTeam.name ]
+                , p [] [ text game.awayTeam.coach.name ]
+                ]
+            ]
+        ]
+
+
+viewDivision : Division -> Html Msg
+viewDivision division =
+    span
+        (Custom.Attributes.textButton <| ViewDivisionButtonClick division.id)
+        [ text <| division.name ++ " Season " ++ String.fromInt division.season ]
+
+
+viewScore : Game -> Html Msg
+viewScore game =
+    case game.homeScore of
+        Just homeScore ->
+            case game.awayScore of
+                Just awayScore ->
+                    text <| String.fromInt homeScore ++ " - " ++ String.fromInt awayScore
+
+                Nothing ->
+                    text "vs"
+
+        Nothing ->
+            text "vs"
+
+
+
+-- View Drop Downs --
+
+
+viewTeamSelector : WebData (List Team) -> Maybe TeamId -> Maybe TeamId -> Html Msg
+viewTeamSelector teamData team1Id team2Id =
+    div Custom.Attributes.row
+        [ viewDropdown team1Id teamData team1Dropdown
+        , text "vs."
+        , viewDropdown team2Id teamData team2Dropdown
+        ]
+
+
+viewDropdown : Maybe id -> WebData (List { obj | id : id, name : comparable }) -> (Maybe id -> List { obj | id : id, name : comparable } -> Html Msg) -> Html Msg
+viewDropdown idData data dropDownFunction =
+    case data of
+        RemoteData.NotAsked ->
+            dropDownFunction Nothing []
+
+        RemoteData.Loading ->
+            h4 [] [ text "Loading Options..." ]
+
+        RemoteData.Failure httpError ->
+            h4 [ Custom.Attributes.errorMessage ]
+                [ text <| "Cannot load Options. " ++ Error.buildErrorMessage httpError ]
+
+        RemoteData.Success d ->
+            dropDownFunction idData <| List.sortBy .name d
+
+
+team1Dropdown : Maybe TeamId -> List Team -> Html Msg
+team1Dropdown selectedTeam teams =
+    div [ Custom.Attributes.col ]
+        [ select
+            (Custom.Attributes.formDropdown "team1Dropdown"
+                [ onInput Team1Selected ]
+            )
+            (defaultOption :: List.map (teamOption selectedTeam) teams)
+        ]
+
+
+team2Dropdown : Maybe TeamId -> List Team -> Html Msg
+team2Dropdown selectedTeam teams =
+    div [ Custom.Attributes.col ]
+        [ select
+            (Custom.Attributes.formDropdown "team2Dropdown"
+                [ onInput Team2Selected ]
+            )
+            (defaultOption :: List.map (teamOption selectedTeam) teams)
+        ]
+
+
+teamOption : Maybe TeamId -> Team -> Html msg
+teamOption currentTeam team =
+    option
+        [ value <| Team.idToString team.id
+        , selected (team.id == Maybe.withDefault defaultTeam.id currentTeam)
+        ]
+        [ text team.name ]
+
+
+defaultOption : Html Msg
+defaultOption =
+    option [ value "0" ] [ text "-" ]
