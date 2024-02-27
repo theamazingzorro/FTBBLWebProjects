3d3e92b4272f5bca007a92a66d424ce93f68f2d0
Fox Noland <theamazingzorro@gmail.com>
2024-02-26 23:43:09 -0500

    game card tweaks

--- Frontend/src/Custom/Html.elm
+++ Frontend/src/Custom/Html.elm
@@ -435,7 +435,7 @@ shadedContainer attributes children =
 
 floatingCard : List (Attribute msg) -> List (Html msg) -> Html msg
 floatingCard attributes children =
-    div (class "w3-card-4 w3-container w3-white floating-card-container round-corners" :: attributes)
+    div (class "w3-card-4 w3-panel w3-white floating-card" :: attributes)
         children
 
 
